938d4ede2f245d2fe11528d7405e6c20970076eb
Okubyona <andrewbazua@gmail.com>
2022-04-24 11:46:33 -0700

    refactor: rename variables for better readability

--- Assets/Scripts/MovePlayers.cs
+++ Assets/Scripts/MovePlayers.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class MovePlayers : MonoBehaviour
 {
-    private Vector3 StartPosition;
-    private Vector3 Destination;
+    private Vector3 initialPosition;
+    private Vector3 destination;
     private float MoveSpeed;
 
     // Grab "player" position
-    private void Awake() {
-        StartPosition = gameObject.transform.position;
+    private void Initialize() {
+        initialPosition = gameObject.transform.position;
         StartCoroutine(ChangeDirection());
     }
 
     // Corutine to randomly change the "player"'s direction every set amount of frames
     private IEnumerator ChangeDirection() {
-        Vector3 initial_Position = new Vector3(StartPosition.x, StartPosition.y, StartPosition.z);
-        Destination = initial_Position += new Vector3(Random.Range(-15, 15), Random.Range(-7, 20));
+        Vector3 initial_Position = new Vector3(initialPosition.x, initialPosition.y, initialPosition.z);
+        destination = initial_Position += new Vector3(Random.Range(-15, 15), Random.Range(-7, 20));
 
         MoveSpeed = Random.Range(5, 15);
 
@@ -28,6 +28,6 @@ public class MovePlayers : MonoBehaviour
     // Update is called once per frame
     void Update() {
         Vector3 position = gameObject.transform.position;
-        gameObject.transform.position = Vector3.MoveTowards(position, Destination, MoveSpeed * Time.deltaTime);
+        gameObject.transform.position = Vector3.MoveTowards(position, destination, MoveSpeed * Time.deltaTime);
     }
 }
