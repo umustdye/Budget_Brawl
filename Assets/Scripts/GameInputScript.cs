a96bd4ebbc1eac32d923f3b20f3efb68a12786e5
Sebastian Hall <sebastianhall13@gmail.com>
2022-04-29 13:14:00 -0700

    Merge input, animation, and player controller in develop

---
+++ Assets/Scripts/GameInputScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameInputScript : MonoBehaviour
+{
+    // Movement
+    [Header("Movement Input")]
+    public float player_direction = 0;
+    public bool sprint = false;
+    public bool jump = false;
+
+    // Fighting
+    [Header("Fighting")]
+    public bool punch = false;
+    public bool kick = false;
+    public bool block = false;
+
+    // Input Action Message Handlers
+    void OnDirection(InputValue val)
+    {
+        player_direction = val.Get<float>();
+        Debug.Log("player_direction: " + player_direction);
+    }
+
+    void OnSprint(InputValue val)
+    {
+        sprint = val.isPressed;
+        Debug.Log("sprint: " + sprint);
+    }
+
+    void OnJump(InputValue val)
+    {
+        jump = val.isPressed;
+        Debug.Log("jump: " + jump);
+    }
+
+    void OnPunch(InputValue val)
+    {
+        punch = val.isPressed;
+        Debug.Log("punch: " + punch);
+    }
+
+    void OnKick(InputValue val)
+    {
+        kick = val.isPressed;
+        Debug.Log("kick: " + kick);
+    }
+
+    void OnBlock(InputValue val)
+    {
+        block = val.Get<float>() != 0;
+        Debug.Log("block: " + block);
+    }
+}
