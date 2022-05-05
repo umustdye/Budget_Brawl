0522e5f25af4aa20a027202181986734c73ae05c
Okubyona <andrewbazua@gmail.com>
2022-05-04 21:18:17 -0700

    docs: add comments to describe HealthBar script's functionality

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,14 +5,17 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    // Controls the 'Fill' gameObject 
     public Slider slider;
 
+    // Changes slider max value to match the passed in hp value
     public void SetMaxHealth(int maxHPValue)
     {
         slider.maxValue = maxHPValue;
         slider.value = maxHPValue;
     }
 
+    // Changes slider value to mach passed in value
     public void SetHealth(int hpValue)
     {
         slider.value = hpValue;
