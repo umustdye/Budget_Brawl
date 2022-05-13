81605232c6f0ca23a762987f2058616a2eb0e16d
Okubyona <andrewbazua@gmail.com>
2022-05-13 12:01:55 -0700

    feat: update playergameinfo to track player death and update health

--- Assets/Scripts/PlayerGameInfo.cs
+++ Assets/Scripts/PlayerGameInfo.cs
@@ -12,8 +12,8 @@ public class PlayerGameInfo : MonoBehaviour
 
     public HealthBar player1HealthBar;
     public HealthBar player2HealthBar;
-    public int player1Health;
-    public int player2Health;
+    public int player1Health = 10000;
+    public int player2Health = 10000;
 
     public int player1Lives = 3;
     public int player2Lives = 3;
@@ -21,6 +21,10 @@ public class PlayerGameInfo : MonoBehaviour
     public int roundTime;
 
     public bool timeOut = false;
+    public bool player1Win = false;
+    public bool player2Win = false;
+    public bool player1Dead = false;
+    public bool player2Dead = false;
 
     public 
 
@@ -34,12 +38,45 @@ public class PlayerGameInfo : MonoBehaviour
 
         player1Lives = 3;
         player2Lives = 3;
+        player1Win = false;
+        player2Win = false;
+        player1Dead = false;
+        player2Dead = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        player1HealthBar.SetHealth(player1Health);
+        player2HealthBar.SetHealth(player2Health);
+
+        if (player1Health <= 0)
+        {
+            player1Dead = true;
+
+            if (player1Lives <= 0)
+            {
+                player2Win = true;
+            }
+            else
+            {
+                //Respawn player 1
+            }
+        }
+        else if (player2Health <= 0)
+        {
+            player2Dead = true;
+
+            if (player1Lives <= 0)
+            {
+                player1Win = true;
+            }
+            else
+            {
+                //Respawn player 2
+            }
+        }
     }
 }
