904e5e8999e52280bd9ff95b0d56d0dfaa8efe39
Okubyona <andrewbazua@gmail.com>
2022-05-29 14:18:23 -0700

    refactor: change respawn points to public Vector3's instead of game objects

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -6,12 +6,16 @@ public class Respawn : MonoBehaviour
 {
 
     public float respawnTimer;
-    public GameObject spawnManager;
-    public List<GameObject> spawnPoints;
     private PlayerGameInfo playerGameInfo;
     private List<GameObject> players;
     private bool isDead;
-    private float timecount = 0;
+
+    // Initialize default values for spawnpoints
+    public Vector3 P1SpawnPoint = Vector3.zero;
+    public Vector3 P2SpawnPoint = Vector3.zero;
+    public Vector3 P3SpawnPoint = Vector3.zero;
+    public Vector3 P4SpawnPoint = Vector3.zero;
+    public List<Vector3> spawnPointPositions;
 
     private IEnumerator respawnCoroutine;
     // Start is called before the first frame update
@@ -19,7 +23,6 @@ public class Respawn : MonoBehaviour
     {
         playerGameInfo = GameObject.FindObjectOfType<PlayerGameInfo>();
         players = playerGameInfo.Players;
-        spawnManager = GameObject.Find("RespawnPoints");
         BuildSpawnPointList();
     }
 
@@ -49,13 +52,14 @@ public class Respawn : MonoBehaviour
 
     IEnumerator RespawnPlayer(GameObject player, int playerIndex)
     {
-        ++timecount;
         Rigidbody playerBody = player.GetComponent(typeof(Rigidbody)) as Rigidbody;
         PlayerController playerController = player.GetComponent(typeof(PlayerController)) as PlayerController;
 
         yield return new WaitForSeconds(respawnTimer);
         
-        player.transform.position = spawnPoints[playerIndex].transform.position;
+        // Move player to spawn point
+        player.transform.position = spawnPointPositions[playerIndex];
+        // Reset scale of player back to default
         player.transform.localScale = new Vector3(1, 1, 1);
 
         // Unfreeze player
@@ -68,15 +72,33 @@ public class Respawn : MonoBehaviour
         player.GetComponent<linkPlayerHealth>().refillFull();
 
         player.GetComponent<linkPlayerHealth>().isRespawning = false;
-        Debug.Log("Visited: " + timecount + " times");
     }
 
     private void BuildSpawnPointList()
     {
-        foreach (Transform child in spawnManager.transform)
-        {
-            spawnPoints.Add(child.gameObject);
-        }
+        spawnPointPositions.Add(P1SpawnPoint);
+        spawnPointPositions.Add(P2SpawnPoint);
+        spawnPointPositions.Add(P3SpawnPoint);
+        spawnPointPositions.Add(P4SpawnPoint);
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Player 1
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(P1SpawnPoint, 0.15f);
+
+        // Player 2
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(P2SpawnPoint, 0.15f);
+
+        // Player 3
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(P3SpawnPoint, 0.15f);
+
+        // Player 4
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(P4SpawnPoint, 0.15f);
     }
 
 }
